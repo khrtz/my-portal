25249418f6936a014fac03122e5b98b8213b3584
khrtz <garula.oze498@gmail.com>
2019-05-29 15:14:16 +0900

    Add material-ui & react

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Home } from './components/home';
+
+ReactDOM.render((
+  <Home />
+), document.getElementById('app'));
