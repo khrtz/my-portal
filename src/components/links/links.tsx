1fae83eb8b91a1a4136925593422186aa8563627
khrtz <garula.oze498@gmail.com>
2019-05-31 19:19:57 +0900

    feat: Add repository link

--- src/components/links/links.tsx
+++ src/components/links/links.tsx
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Profile from '../profile/profile';
 import Link from '@material-ui/core/Link';
+import { Chip, Avatar, Box } from '@material-ui/core';
+import { CodeRounded } from '@material-ui/icons';
 
 interface Props {
 }
@@ -10,6 +12,9 @@ interface Props {
 const useStyles = makeStyles({
   link: {
     margin: 5
+  },
+  repoLink: {
+    marginLeft: '6px'
   }
 });
 
@@ -20,11 +25,29 @@ export const Links: React.SFC<Props> = ({
   return (
     <div>
       <Profile />
-      <Typography color='secondary' variant={'subtitle1'}>
-        <Link href="https://blog.khrtz.xyz" className={classes.link}>Blog</Link>
-        <Link href="https://github.com/khrtz" className={classes.link}>GitHub</Link>
-        <Link href="https://twitter.com/khrtz" className={classes.link}>Twitter</Link>
-      </Typography>
+      <Box display="flex" alignItems="center">
+        <Typography color='secondary' variant={'subtitle1'}>
+          <Link href="https://blog.khrtz.xyz" className={classes.link}>Blog</Link>
+          <Link href="https://github.com/khrtz" className={classes.link}>GitHub</Link>
+          <Link href="https://twitter.com/khrtz" className={classes.link}>Twitter</Link>
+          <Link
+            className={classes.repoLink}
+            href="https://github.com/khrtz/my-portal"
+          >
+            <Chip
+              color="secondary"
+              avatar={
+                <Avatar>
+                  <CodeRounded />
+                </Avatar>
+              }
+            label="This Site Code"
+            variant="default"
+            clickable
+          />
+        </Link>
+        </Typography>
+      </Box>
     </div>
   );
 };
