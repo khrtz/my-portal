c977477bacdda3b6d51916332241b9303d554792
khrtz <garula.oze498@gmail.com>
2019-05-31 19:28:14 +0900

    feat(RepoLink): Move to AppBar

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -5,12 +5,16 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography'
 
 import { Links } from '../links';
-import { Container } from '@material-ui/core';
+import { Container, Chip, Avatar, Link } from '@material-ui/core';
+import { CodeRounded } from '@material-ui/icons';
 
 const useStyles = makeStyles({
   root: {
     flexGrow: 1,
   },
+  title: {
+    flexGrow: 1,
+  }
 });
 
 function SimpleAppBar() {
@@ -20,9 +24,24 @@ function SimpleAppBar() {
     <div className={classes.root}>
       <AppBar position="static" color="primary">
         <Toolbar>
-          <Typography variant="h6" color="initial">
+          <Typography className={classes.title} variant="h6" color="initial">
             khrtz
           </Typography>
+          <Link
+            href="https://github.com/khrtz/my-portal"
+          >
+            <Chip
+              color="primary"
+              avatar={
+                <Avatar>
+                  <CodeRounded />
+                </Avatar>
+              }
+            label="This Site Code"
+            variant="default"
+            clickable
+          />
+        </Link>
         </Toolbar>
       </AppBar>
     </div>
